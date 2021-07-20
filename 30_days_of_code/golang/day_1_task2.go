ec7cf629fe40c42b5e32159c64e361a4b92ced67
Salavat Bibarsov <Bibarsov.Salavat@gmail.com>
2021-07-20 11:05:29 +0300

    Add solved task of 30 days of code

---
+++ 30_days_of_code/golang/day_1_task2.go
@@ -0,0 +1,92 @@
+package main
+
+// Day 1: Data Types
+// https://www.hackerrank.com/challenges/30-data-types/problem
+
+/*
+ * Objective
+ * Today, we're discussing data types. Check out the Tutorial tab for learning materials and an instructional video!
+ *
+ * Task
+ * Complete the code in the editor below. The variables i, d, and s are already declared and initialized for you.
+ * You must:
+ *  1. Declare 3 variables: one of type int, one of type double, and one of type String.
+ *  2. Read 3 lines of input from stdin (according to the sequence given in the Input Format section below) and
+ *  initialize your 3 variables.
+ *  3. Use the + operator to perform the following operations:
+ *      1. Print the sum of i plus your int variable on a new line.
+ *      2. Print the sum of d plus your double variable to a scale of one decimal place on a new line.
+ *      3. Concatenate s with the string you read as input and print the result on a new line.
+ *
+ * Note: If you are using a language that doesn't support using + for string concatenation (e.g.: C),
+ * you can just print one variable immediately following the other on the same line. The string provided in your
+ * editor must be printed first, immediately followed by the string you read as input.
+ *
+ * Input Format
+ * The first line contains an integer that you must sum with i.
+ * The second line contains a double that you must sum with d.
+ * The third line contains a string that you must concatenate with s.
+ *
+ * Output Format
+ * Print the sum of both integers on the first line, the sum of both doubles (scaled to 1 decimal place) on
+ * the second line, and then the two concatenated strings on the third line.
+ *
+ * Sample Input
+ * >>> 12
+ * >>> 4.0
+ * >>> is the best place to learn and practice coding!
+ *
+ * Sample Output
+ * <<< 16
+ * <<< 8.0
+ * <<< HackerRank is the best place to learn and practice coding!
+ *
+ * Explanation
+ * When we sum the integers 14 and 12, we get the integer 16.
+ * When we sum the floating-point numbers 4.0 and 4.0, we get 8.0.
+ * When we concatenate HackerRank with is the best place to learn and practice coding!,
+ * we get HackerRank is the best place to learn and practice coding!.
+ *
+ * You will not pass this challenge if you attempt to assign the Sample Case values to your variables
+ * instead of following the instructions above and reading input from stdin.
+ *
+*/
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+  
+	var i uint64 = 4
+	var d float64 = 4.0
+	var s string = "HackerRank "
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// Declare second integer, double, and String variables.
+	var myI uint64
+	var myD float64
+	var myS string
+
+	// Read and save an integer, double, and String to your variables.
+	fmt.Scanf("%d", &myI)
+	fmt.Scanf("%f", &myD)
+
+	if scanner.Scan() {
+		myS = scanner.Text()
+	}
+
+	// Print the sum of both integer variables on a new line.
+	fmt.Println(i + myI)
+
+	// Print the sum of the double variables on a new line.
+	fmt.Printf("%.1f\n", d + myD)
+
+	// Concatenate and print the String variables on a new line
+	// The 's' variable above should be printed first.
+	fmt.Println(s + myS)
+
+}
--- README.md
+++ README.md
@@ -12,7 +12,8 @@ My solved tasks in 30 Days of Code challenge.
 
 № | Title | Difficulty |Score | C | Python | Go
 --|-------|------------|------|---|--------|---
-1 | [Day 0: Hello, World](https://www.hackerrank.com/challenges/30-hello-world/problem) | Easy | 30.0 | [C](https://github.com/BibarsovSalavat/hackerrank/blob/main/30_day_of_code/c/day_0_task1.c) | [Python](https://github.com/BibarsovSalavat/hackerrank/blob/main/30_day_of_code/python/day_0_task1.py) | [Go](https://github.com/BibarsovSalavat/hackerrank/blob/main/30_day_of_code/golang/day_0_task1.go)
+1 | [Day 0: Hello, World](https://www.hackerrank.com/challenges/30-hello-world/problem) | Easy | 30.0 | [C](https://github.com/BibarsovSalavat/hackerrank/blob/main/30_days_of_code/c/day_0_task1.c) | [Python](https://github.com/BibarsovSalavat/hackerrank/blob/main/30_days_of_code/python/day_0_task1.py) | [Go](https://github.com/BibarsovSalavat/hackerrank/blob/main/30_days_of_code/golang/day_0_task1.go)
+2 | [Day 1: Data Types](https://www.hackerrank.com/challenges/30-data-types/problem) | Easy | 30.0 | [C](https://github.com/BibarsovSalavat/hackerrank/blob/main/30_days_of_code/c/day_1_task2.c) | [Python](https://github.com/BibarsovSalavat/hackerrank/blob/main/30_days_of_code/python/day_1_task2.py) | [Go](https://github.com/BibarsovSalavat/hackerrank/blob/main/30_days_of_code/golang/day_1_task2.go)
 
 ## **`Python language`**
 
